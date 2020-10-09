fd188aa161c9cb7d1924b22fe5c423d8417b6484
Max Börebäck <max@smstid.se>
2020-10-09 18:03:34 +0200

    Add files via upload
    Added speed calculation

--- arduino/bback_car_sensors.ino
+++ arduino/bback_car_sensors.ino
@@ -3,13 +3,19 @@
 long duration;
 int distance;
 int tmpDistance;
-int maxUcDistance = 400;
+int maxUsDistance = 400;
+long currentTime = millis();
 
+int bumperPinArray[] = {9}; 
 int trigPinArray[] = {5, 7, 3}; // F,L,R
 int echoPinArray[] = {6, 8, 4};
-int usLastDistance[] = {maxUcDistance, maxUcDistance, maxUcDistance}; // F,L,R
+int usLastDistance[] = {maxUsDistance, maxUsDistance, maxUsDistance};// F,L,R
+long usLastTimeStamp[] = {currentTime,currentTime,currentTime}; // -1 indicates no previous time
+double usSpeed[] = {-1,-1,-1}; // Current speed,-1 indicates unknowm speed 
 int numberOfUs = 3;
-int numberOfIr = 3;
+int numberOfIr = 0;
+int numberOfBumper = 0;
+
 
 #define model 1080
 
@@ -34,27 +40,100 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   //Serial.println(analogRead(A0));
+  
   String s = "0";
+  String bs = "1"; // If bumped
+  int toClose = 0;
+  int bumped = 0;
+  int speed = 0;
+  for (int i = 0; i < numberOfBumper; i = i + 1) {
+    bumped = getBumper(bumperPinArray[i]);
+    s = s + ":" + String(bumped);
+    bs = bs + ":" + String(bumped);
+    if (bumped == 1) {
+      toClose = 1;
+    }
+  }
+
+  if (toClose == 1) { // Sent a emrgency stop
+    bs = s;
+    for (int i = 0; i < numberOfUs; i = i + 1) {
+      bs = bs + ":0:0";
+    }
+    for (int i = 0; i < numberOfIr; i = i + 1) {
+     bs = bs + ":0";
+    }
+    Serial.println(bs);
+  }
   
   for (int i = 0; i < numberOfUs; i = i + 1) {
     int distance = getUsDistance(trigPinArray[i],echoPinArray[i]);
-    if (distance >= maxUcDistance) {
+    currentTime = millis();
+    if (distance >= maxUsDistance) {
       distance = usLastDistance[i];
-    } 
+    }
+    speed = getUsSpeed(i,distance);
     usLastDistance[i] = distance;
     s = s + ":" + String(distance);
+    s = s + ":" + String(speed);
   }
+  
   for (int i = 0; i < numberOfIr; i = i + 1) {
     s = s + ":" + String(getIrDistance(i));
   }
+  
   Serial.println(s);
-  //delay(200);
 }
 
 int getIrDistance(int irNr) {
-  // 10-80 cm
+  int irvalues[25];
   int result = irSensorArray[irNr].distance();
+
+  //int result = 0;
+  //for (int i = 0; i < 25; i = i + 1) {
+  //  irvalues[i] = analogRead(irNr);
+  //}
+   // Number of items in the array
+  //int lt_length = sizeof(irvalues) / sizeof(irvalues[0]);
+  // qsort - last parameter is a function pointer to the sort function
+  //qsort(irvalues, lt_length, sizeof(irvalues[0]), sort_desc);
+  
+  //result = irvalues[13];
   return result;
+}
+
+int getUsSpeed(int sensor, int distance) {
+  int deltaDistance = 0;
+  int speed = -1;
+  //delay(100);
+
+  if (usLastDistance[sensor] == maxUsDistance) {
+    speed = -1;
+  } else {
+    if (currentTime < usLastTimeStamp[sensor]) {
+      // timer has reset, restarting
+      speed = -1;
+    } else {
+      deltaDistance = abs(usLastDistance[sensor] - distance)*1000;
+      //Serial.println(deltaDistance);
+      //Serial.println(currentTime - usLastTimeStamp[sensor]);
+      if (deltaDistance > 0) {       
+        speed = deltaDistance / (currentTime - usLastTimeStamp[sensor]);
+      } else {
+        speed = 0;
+      }
+    }
+  }
+  usSpeed[sensor] = speed;
+  
+  usLastTimeStamp[sensor] = currentTime;
+  //Serial.print(speed);
+    
+  return speed; // Distance in CM/milisecond
+}
+
+int getBumper(int pin) {
+  return digitalRead(pin);
 }
 
 int getUsDistance(int trigPin, int echoPin) {
@@ -68,4 +147,17 @@ int getUsDistance(int trigPin, int echoPin) {
   distance = duration * 0.034 / 2;
     
   return distance; // Distance in CM
+}
+
+
+// qsort requires you to create a sort function
+int sort_desc(const void *cmp1, const void *cmp2)
+{
+  // Need to cast the void * to int *
+  int a = *((int *)cmp1);
+  int b = *((int *)cmp2);
+  // The comparison
+  return a > b ? -1 : (a < b ? 1 : 0);
+  // A simpler, probably faster way:
+  //return b - a;
 }
